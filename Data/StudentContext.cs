74cfe19c3bacfa861a07b0728661ef9b522d061b
Angel <ngeljames2409@gmail.com>
2021-01-04 21:06:41 +0530

    final commit

---
+++ Data/StudentContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication10;
+
+namespace WebApplication10.Data
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext (DbContextOptions<StudentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication10.Student> Student { get; set; }
+    }
+}
